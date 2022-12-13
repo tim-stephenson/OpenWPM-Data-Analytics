a094b975bc2e32b08f6cd02075d1259b210c7d5b
Timothy Stephenson <38039215+tim-stephenson@users.noreply.github.com>
2022-12-13 17:12:56 -0500

    worked on regular expressions a bit

--- main/functions/static_analysis.py
+++ main/functions/static_analysis.py
@@ -1,19 +1,18 @@
 
 import ast
 import math
+import re
 import sqlite3
 import subprocess
 from typing import Dict, List, Set, Tuple, Union
 import pandas as pd
+import jsbeautifier
 
 # (visit_id, script_url)
 Identifier = Tuple[str,str]
 
 # returns a dict[ fingerprinting method ] = [ (visit_id,script_url), ... ]
 def Static(con : sqlite3.Connection, db : any) -> Dict[str, List[Identifier] ]:
-    # build
-    subprocess.run(["sh", "node/run.sh", "npm run build"])
-
     numEntries : sqlite3.Cursor = con.cursor().execute(
         """SELECT COUNT(visit_id)
         FROM http_responses 
@@ -31,16 +30,41 @@ def Static(con : sqlite3.Connection, db : any) -> Dict[str, List[Identifier] ]:
     }
     i : int = 0
     for row in responses:
-        if(i % 10000 == 0):
+        if(i % 100 == 0):
             print(f"Done: {i} out of {n}")
         i += 1
 
-        code : str = db.get( bytes( row["content_hash"], encoding="ascii" ) )
-        # completedProcess : subprocess.CompletedProcess = subprocess.run(["sh", f"node/run.sh \"npm run execute \"{code}\"\""])
-        code : str = "1+1"
-        completedProcess : subprocess.CompletedProcess = subprocess.run(["sh", "node/run.sh", f"npm run execute \"{code}\""])
+        code : str = str( db.get( bytes( row["content_hash"], encoding="ascii" ) ),encoding="UTF-8")
         id : Identifier = (row["visit_id"], row["url"] )
-        if completedProcess.returncode == 1:
-            results["Canvas"].append(id)
-            print(f"Found: {id}")
+        Analyze(code, results, id)
     return results
+
+def Analyze(code : str, results : Dict[str, List[Identifier] ], id : Identifier ) -> None:
+    code = jsbeautifier.beautify(code)
+    if Canvas(code):
+        results["Canvas"].append(id)
+        print(f"Found: {id}")
+
+def Canvas(code : str):
+    try:
+        # m : re.Match = re.search("toDataURL",code)
+        # print(m)
+        # if m.lastindex != None:
+        #   for g in range(m.lastindex + 1):
+        #       print( m.string[(m.start(g)-10):(m.end(g) + 10)] )
+        for m in re.finditer("toDataURL",code):
+            print("-"*30)
+            print( m.string[(m.start(0)-10):(m.end(0) + 10)] )
+            print("-"*30)
+    except Exception as e:
+        print(f"Found Exception {e}")
+
+    return True
+
+
+"""
+    # build
+    subprocess.run(["sh", "node/run.sh", "npm run build"])
+    completedProcess : subprocess.CompletedProcess = subprocess.run(["sh", "node/run.sh", f"npm run execute \"{code}\""])
+
+"""
