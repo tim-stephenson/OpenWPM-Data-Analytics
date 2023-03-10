237a1f0a6eba1201d3fb7290d7b612af4da45d6a
Timothy Stephenson <38039215+tim-stephenson@users.noreply.github.com>
2023-03-10 15:34:09 -0500

    simplefied what used to be control.py, which is now, run_analysis

--- main/run_analysis.py
+++ main/run_analysis.py
@@ -5,7 +5,7 @@ from typing import List, Any
 import plyvel #type: ignore
 from pathlib import Path
 from analyzers.analyzer import Analyzer
-from utils.into_table_utils import analyzerObjects_to_dataframe, dataframe_to_table, dataframe_to_analyzerObjects,table_to_dataframe, table_exists, PROTECTED_TABLE_NAMES
+from utils.into_table_utils import analyzerObjects_to_dataframe, dataframe_to_table, PROTECTED_TABLE_NAMES
 from utils.runHealth import runHealth
 
 import argparse
@@ -22,8 +22,6 @@ if __name__ == '__main__':
         help="path of datadir directory")
     parser.add_argument('--leveldb', type=str, action='store',
         help='Name of LevelDB created by OpenWPM', default='leveldb')
-    parser.add_argument("--from_cache",  action="store_true",
-        help="use analysis data from previous run", default=False)
     parser.add_argument('--analyzers', type=str, action='store',
         help='Comma-separated list of names of analyzer classes to use', default="")
     parser.add_argument("--table_name", type=str, action="store",
@@ -35,16 +33,12 @@ if __name__ == '__main__':
     engine : Engine = create_engine(f"""sqlite:///{path.joinpath("crawl-data.sqlite")}""")
     db : Any = plyvel.DB( str(path.joinpath(args.leveldb)) ) # type: ignore
 
-    load_from_cache : bool = args.from_cache
     table_name : str = args.table_name
     if table_name in PROTECTED_TABLE_NAMES:
         logger.error(f"""Cannot use a table name used by the OpenWPM output data.
 Your table name: {table_name}
 The table names used by OpenWPM:\n{PROTECTED_TABLE_NAMES}""")
         sys.exit(1)
-    if load_from_cache and not table_exists(table_name,engine):
-        load_from_cache = False
-        logger.warning("Program was run with '--from-cache' yet no cache exits. Program is ignoring --from_cache flag.")
 
     n,f = runHealth(engine)
     logger.info(f"total visits: {n}, failed/incomplete visits: {f}. Success percentage: {round(100* (1 - f/n)) }%")
@@ -54,14 +48,10 @@ The table names used by OpenWPM:\n{PROTECTED_TABLE_NAMES}""")
     else:
         analyzer_objects: List[Analyzer] = analyzers_from_class_names(str(args.analyzers).split(','), engine,db,logger)
 
-    if load_from_cache:
-        df : pandas.DataFrame =  table_to_dataframe(engine,table_name)
-        dataframe_to_analyzerObjects(analyzer_objects,df)
-    else:
-        run_analyzers(analyzer_objects)
-        df : pandas.DataFrame = analyzerObjects_to_dataframe(analyzer_objects)
-        dataframe_to_table(df,engine,table_name)
 
-    # get_all_symmetric_differences(analyzer_objects, logger)
+    run_analyzers(analyzer_objects)
+    df : pandas.DataFrame = analyzerObjects_to_dataframe(analyzer_objects)
+    dataframe_to_table(df,engine,table_name)
+
 
     
