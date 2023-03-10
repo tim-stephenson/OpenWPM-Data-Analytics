1c2ba5f17dd2a6b1f09ecb5ef2dede3e9f2c7c35
Timothy Stephenson <38039215+tim-stephenson@users.noreply.github.com>
2023-03-10 15:26:56 -0500

    update scripts

--- scripts/micromamba-install.sh
+++ scripts/micromamba-install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ -z "$BASH_VERSION" ]; then
+    echo "This script is required to be ran with the bash shell via bash -i SCRIPT"
+    exit 1
+fi
+if [ -z "$PS1" ]; then
+    echo "This script is required to be ran interactivly via bash -i SCRIPT"
+    exit 1
+fi
+
 wget micro.mamba.pm/install.sh -O - | bash -
 source ~/.bashrc
 echo "successfully installed micromamba $(micromamba --version)"
