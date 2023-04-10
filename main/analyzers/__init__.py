d664627c5f4df0205f37041987c7f79e4b8cb0f5
Timothy Stephenson <38039215+tim-stephenson@users.noreply.github.com>
2023-04-10 15:52:23 -0400

    added Canvas_Basic_Static_2 which un-escaped escaped charecters

---
+++ main/analyzers/__init__.py
@@ -0,0 +1,17 @@
+from .static_analyzers.canvas_1m_static import Canvas_1M_Static
+from .static_analyzers.canvas_basic_static import Canvas_Basic_Static
+from .static_analyzers.canvas_basic_static_2 import Canvas_Basic_Static_2
+from .static_analyzers.canvas_font_1m_static import Canvas_Font_1M_Static
+from .static_analyzers.media_queries_static import Media_Queries_Static
+from .static_analyzers.navigator_properties_static import Navigator_Properties_Static
+from .static_analyzers.webgl_static import WebGL_Static
+from .static_analyzers.webrtc_1m_static import WebRTC_1M_Static
+
+from .dynamic_analyzers.canvas_1m_dynamic_nd import Canvas1MDynamicND
+from .dynamic_analyzers.canvas_1m_dynamic import Canvas_1M_Dynamic
+from .dynamic_analyzers.canvas_basic_dynamic import Canvas_Basic_Dynamic
+from .dynamic_analyzers.canvas_font_1m_dynamic import Canvas_Font_1M_Dynamic
+from .dynamic_analyzers.media_queries_dynamic import Media_Queries_Dynamic
+from .dynamic_analyzers.navigator_properties_dynamic import Navigator_Properties_Dynamic
+from .dynamic_analyzers.webgl_dynamic import WebGL_Dynamic
+from .dynamic_analyzers.webrtc_1m_dynamic import WebRTC_1M_Dynamic
